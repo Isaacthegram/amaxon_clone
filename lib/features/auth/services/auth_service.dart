4e4274cf679e3808c9980bb081c452ee989e6384
Worldtimeapp <jaydenanireh@gmail.com>
2023-12-28 09:03:10 +0100

    2nd COMMIT

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -37,7 +37,7 @@ class AuthService {
             );  
 
             http.Response res = await http.post(
-                Uri.parse('$uri/api/signin'),
+                Uri.parse('$uri/api/signup'),
                 body: user.toJson(),
                 headers: <String, String>{
                     'Content-Type': 'application/json; charset=UTF-8',
@@ -59,4 +59,84 @@ class AuthService {
             showSnackBar(context, e.toString());
         }
     }
+
+  //sign up user
+  void signInUser({
+    required BuildContext context,
+    required String email,
+    required String password,
+
+  }) async {
+    try {
+      User user = User(
+        id: '',
+        name: '',
+        password: password,
+        email: email,
+        address: '',
+        type: '',
+        token: '',
+      );
+
+      http.Response res = await http.post(
+        Uri.parse('$uri/api/signin'),
+        body: user.toJson(),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+
+      httpErrorHandle(
+        response: response,
+        context: context,
+        onSuccess: () async {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+          await prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
+          Navigator.pushAndRemoveUntil(context, HomeScreen.routeName as Route<Object?>, (route) => false);
+
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+
+  //get user data
+  void getUserData({
+     required BuildContext context,
+  }) async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString('x-auth-token');
+
+      if(token == null) {
+        prefs.setString('x-auth-token', '');
+      }
+
+      var tokenRes = await http.post(
+        Uri.parse('$uri/tokenIsValid'),
+        headers: <String, String> {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': token!
+        },
+      );
+      var response = jsonDecode(tokenRes.body);
+
+      if(response == true) {
+        http.Response userRes = await http.get(Uri.parse('$uri/'),
+          headers: <String, String> {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': token
+        },
+        );
+
+        var userProvider = Provider.of<UserProvider>(context, listen: false);
+        userProvider.setUser(userRes.body);
+      }
+
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:amaxonclone/constants/global_variables.dart';
 import 'package:amaxonclone/features/auth/screens/auth_screen.dart';
+import 'package:amaxonclone/features/auth/services/auth_service.dart';
+import 'package:amaxonclone/features/home/screens/home_screen.dart';
 import 'package:amaxonclone/providers/user_provider.dart';
 import 'package:amaxonclone/router.dart';
 import 'package:flutter/material.dart';
@@ -12,9 +14,21 @@ void main() {
       child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService authService = AuthService();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    authService.getUserData(context: context);
+  }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -36,7 +50,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       onGenerateRoute: (settings) => generateRoute(settings),
-      home: const AuthScreen(),
+      home: Provider.of<UserProvider>(context).user.token.isNotEmpty ? const HomeScreen() : const AuthScreen(),
     );
   }
 }
